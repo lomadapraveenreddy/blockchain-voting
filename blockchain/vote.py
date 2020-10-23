58c835e8b03ddcffa21362be04385f1b97efd283
lomadapraveenreddy <lomadapraveenreddy.181co230@nitk.edu.in>
2020-10-23 15:34:26 +0530

    added eq lt gt repr methods in vote class

--- blockchain/vote.py
+++ blockchain/vote.py
@@ -6,3 +6,38 @@ class Vote:
     def __init__(self,pollID,option):
         self.pollID = pollID
         self.option = option
+
+    def __repr__(self):
+        return f'vote-({self.pollID} {self.option})'
+
+
+    def __eq__(self,other):
+        if self.pollID == other.pollID and self.option == other.option:
+            return True
+        return False
+
+    def __lt__(self, other):
+        if self.pollID<other.pollID:
+            return True
+        elif self.pollID>other.pollID:
+            return False
+        else:
+            if self.option<=other.option:
+                return True
+            else:
+                return False
+    def __gt__(self, other):
+        if self.pollID>other.pollID:
+            return True
+        elif self.pollID<other.pollID:
+            return False
+        else:
+            if self.option>other.option:
+                return True
+            else:
+                return False
+
+if __name__ == '__main__':
+    a=Vote('123','2')
+    b=Vote('123','1')
+    print(sorted([a,b]))
