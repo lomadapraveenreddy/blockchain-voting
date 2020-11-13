8fabe9bcaa0b1f4e3740171783bbc3f680ae4e45
lomadapraveenreddy <lomadapraveenreddy.181co230@nitk.edu.in>
2020-11-13 12:14:32 +0530

    made changes

---
+++ wallet/transaction_pool.py
@@ -0,0 +1,12 @@
+class TransactionPool:
+
+    def __init__(self):
+        self.transactionMap = {}
+
+    def setTransaction(self, transaction):
+        self.transactionMap[transaction.transactionID]=transaction
+
+    def existingTransaction(self,address):
+
+        for transaction in self.transactionMap.values():
+            if address == transaction.input['senderAddress']:
